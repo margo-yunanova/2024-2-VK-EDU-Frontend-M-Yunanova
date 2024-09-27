36575c40b7e743d46dfee1eded5de05562ef33b4
margo <m.yunanova@gmail.com>
2024-09-27 20:07:41 +0300

    feat: add markup for chat

--- simple-chat/main.js
+++ simple-chat/main.js
@@ -1,24 +1,43 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import icon from './images/done_all.svg';
+import { vacationChat } from './mock';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
+const formMessage = form.querySelector('.form__message');
+const input = formMessage.querySelector('.form-input');
+const mirrorInput = formMessage.querySelector('.form-input-mirror-text');
+const messages = document.querySelector('.messages');
 
-setupCounter(document.querySelector('#counter'))
+const messageTemplate = document
+  .querySelector('#message-template')
+  .content.querySelector('.message');
+
+input.addEventListener('input', (event) => {
+  mirrorInput.textContent = event.target.value;
+  input.style.height = mirrorInput.offsetHeight + 'px';
+});
+
+const createMessage = ({ id, type, sender, message, timestamp, status }) => {
+  const messageElement = messageTemplate.cloneNode(true);
+
+  messageElement.querySelector('.message-text').textContent = message;
+  messageElement.querySelector('.message-time').textContent =
+    new Intl.DateTimeFormat('ru', {
+      hour: 'numeric',
+      minute: 'numeric',
+    }).format(new Date(timestamp));
+
+  if (type === 'input') {
+    messageElement.classList.add('message_input');
+  }
+
+  if (type === 'input' && status === 'read') {
+    messageElement.querySelector('.message-status').src = icon;
+  }
+
+  return messageElement;
+};
+
+for (const message of vacationChat) {
+  messages.append(createMessage(message));
+}
