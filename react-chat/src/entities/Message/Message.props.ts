468ae84f7e13542a90dd8a4ff0adc8df0157cec8
margo <m.yunanova@gmail.com>
2024-10-24 23:21:10 +0300

    feat: add Message component; ДЗ 5 #6

--- react-chat/src/entities/Message/Message.props.ts
+++ react-chat/src/entities/Message/Message.props.ts
@@ -1,9 +1,13 @@
-export type MessageStatus = 'read' | 'sent';
+import { RefObject } from 'react';
+
+import { MessageStatus } from '@/pages/ChatPage/mock';
+
 export type MessageType = 'input' | 'output';
 
 export interface IMessageProps {
   message: string;
-  timestamp: string;
+  timestamp: Date;
   status: MessageStatus;
   type: MessageType;
+  ref: RefObject<HTMLDivElement> | undefined;
 }
--- react-chat/src/entities/Message/Message.tsx
+++ react-chat/src/entities/Message/Message.tsx
@@ -1,5 +1,5 @@
 import { Check, DoneAll } from '@mui/icons-material';
-import { FC } from 'react';
+import { FC, forwardRef } from 'react';
 
 import { formateDate } from '../../utils/utils';
 import styles from './Message.module.scss';
@@ -15,25 +15,22 @@ const IconsStatus = {
   sent: DoneAll,
 };
 
-export const Message: FC<IMessageProps> = ({
-  message,
-  timestamp,
-  status,
-  type,
-}) => {
-  const IconStatus = IconsStatus[status];
+export const Message: FC<IMessageProps> = forwardRef(
+  ({ message, timestamp, status, type }, ref) => {
+    const IconStatus = IconsStatus[status];
 
-  return (
-    <div className={styles.message} data-type={type}>
-      <p className={styles['message-text']}>{message}</p>
-      <div className={styles['message-info']}>
-        <span className={styles['message-time']}>
-          {formateDate(timestamp, 'ru', timeFormatOptions)}
-        </span>
-        <div className={styles['message-status']}>
-          <IconStatus />
+    return (
+      <div className={styles.message} data-type={type} ref={ref}>
+        <p className={styles['message-text']}>{message}</p>
+        <div className={styles['message-info']}>
+          <span className={styles['message-time']}>
+            {formateDate(timestamp, 'ru', timeFormatOptions)}
+          </span>
+          <div className={styles['message-status']}>
+            <IconStatus />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
