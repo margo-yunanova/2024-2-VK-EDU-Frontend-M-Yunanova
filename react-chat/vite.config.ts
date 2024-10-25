4966b342e3df5394746d717d45f9bc7ccfb94d1a
margo <m.yunanova@gmail.com>
2024-10-26 01:58:25 +0300

    fix: deploy

--- react-chat/vite.config.ts
+++ react-chat/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2024-2-VK-EDU-Frontend-M-Yunanova/',
   plugins: [react()],
   resolve: {
     alias: {
