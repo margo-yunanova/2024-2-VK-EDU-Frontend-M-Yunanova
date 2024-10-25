d940a24e7d1f8c7f30a0e2a85bace758cc9f44a3
margo <m.yunanova@gmail.com>
2024-10-26 01:46:38 +0300

    feat: add ChatsPage

--- react-chat/src/widgets/ChatsPageHeader/ChatsPageHeader.tsx
+++ react-chat/src/widgets/ChatsPageHeader/ChatsPageHeader.tsx
@@ -1,20 +1,23 @@
 import { Menu, Search } from '@mui/icons-material';
 
 import { Header } from '@/entities/Header/Header';
+import { Ripple } from '@/feature/Riiple/Ripple';
 
 import styles from './ChatsPageHeader.module.scss';
 
 export const ChatsPageHeader = () => {
   return (
     <Header extraClassName={styles.header}>
-      <nav className="settings">
+      <nav className={styles.settings}>
         <button className={styles.icon}>
           <Menu />
+          <Ripple color={'#af5dfc'} duration={2000} />
         </button>
       </nav>
       <p className={styles['header-title']}>Messenger</p>
       <button className={styles.icon}>
         <Search />
+        <Ripple color={'#af5dfc'} duration={2000} />
       </button>
     </Header>
   );
