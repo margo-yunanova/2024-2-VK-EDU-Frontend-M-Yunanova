84aec3d82abed758ad2907113b6b61125b3965c1
margo <m.yunanova@gmail.com>
2024-12-09 13:47:04 +0300

    fix: eslint error; #19

--- react-chat/src/pages/CreatingPrivateChatPage/CreatingPrivateChatPage.tsx
+++ react-chat/src/pages/CreatingPrivateChatPage/CreatingPrivateChatPage.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDebounce } from 'use-debounce';
 
