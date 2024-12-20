d3aa1ca645a66a47ef7a82f3dd4bad166bad6a12
margo <m.yunanova@gmail.com>
2024-12-20 13:29:13 +0300

    feat: implement fetch mock for tests

--- react-chat/src/shared/utils/translate.test.ts
+++ react-chat/src/shared/utils/translate.test.ts
@@ -1,10 +1,48 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
 
-import { translate } from './translate';
+import { ITranslatedResponse, translate } from './translate';
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
 
 describe('translate', () => {
-  test('should return translated text', async () => {
-    const response = await translate('Hello world', 'en', 'ru');
-    expect(response).toBe('Привет мир!');
+  afterEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  const response: ITranslatedResponse = {
+    responseData: {
+      translatedText: 'Привет мир!',
+    },
+  };
+
+  test('should call fetch', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(response), {
+      status: 200,
+    });
+
+    const result = await translate('Hello world', 'en', 'ru');
+
+    expect(fetchMock).toBeCalledTimes(1);
+    expect(result).toBe('Привет мир!');
+  });
+
+  test('should call fetch with correct search params', async () => {
+    fetchMock.mockResponse(JSON.stringify(response), {
+      status: 200,
+    });
+
+    const result = await translate('Hello world', 'en', 'ru');
+
+    const result2 = await translate('Hello ', 'en', 'ru');
+
+    expect(fetchMock).toBeCalled();
+    expect(fetchMock).toBeCalledWith(
+      'https://api.mymemory.translated.net/get?q=Hello+world&langpair=en%7Cru',
+    );
+    expect(fetchMock).toMatchSnapshot();
+    expect(result).toBe('Привет мир!');
+    expect(result2).toBe('Привет мир!');
   });
 });
