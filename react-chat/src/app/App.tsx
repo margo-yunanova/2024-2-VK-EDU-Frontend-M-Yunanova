2ad4144f3ca654c2692e19598962f437377b587d
margo <m.yunanova@gmail.com>
2024-12-21 17:21:16 +0300

    fix: reconnect to centrifugo after changing chat; #23

--- react-chat/src/app/App.tsx
+++ react-chat/src/app/App.tsx
@@ -1,48 +1,27 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { useAppDispatch, useAppSelector } from '@/shared/hooks/stateHooks';
 import { useCurrentUser } from '@/shared/hooks/useCurrentUser';
 import { connect } from '@/shared/utils/init';
-import {
-  useCentrifugoConnectCreateMutation,
-  useCentrifugoSubscribeCreateMutation,
-} from '@/store/api';
 
 function App() {
   const currentUser = useCurrentUser();
-  const activeChatId = useAppSelector((state) => state.chat.activeChat?.id);
-  const [getTokenForConnection] = useCentrifugoConnectCreateMutation();
-  const [getTokenForSubscription] = useCentrifugoSubscribeCreateMutation();
-  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const unmounter = connect(
-      async () => (await getTokenForConnection()).data!.token,
-      async () => (await getTokenForSubscription()).data!.token,
-      currentUser.id,
-      activeChatId,
-      dispatch,
-    );
+    const unmounter = connect(currentUser.id);
 
     return () => {
       unmounter();
     };
-  }, [
-    currentUser,
-    getTokenForConnection,
-    getTokenForSubscription,
-    activeChatId,
-    dispatch,
-  ]);
+  }, [currentUser.id]);
 
   useEffect(() => {
     if (!('Notification' in window)) {
-      alert('This browser does not support desktop notification');
+      console.warn('This browser does not support desktop notification');
       return;
     }
 
-    if (Notification.permission !== 'denied') {
+    if (Notification.permission === 'default') {
       Notification.requestPermission();
     }
   }, []);
--- react-chat/src/store/store.ts
+++ react-chat/src/store/store.ts
@@ -19,6 +19,16 @@ const store = configureStore({
 
 setupListeners(store.dispatch);
 
+export const getActiveChatId = () => store.getState().chat.activeChat?.id;
+
+export const getTokenForConnection = () =>
+  store.dispatch(enhancedApi.endpoints.centrifugoConnectCreate.initiate());
+
+export const getTokenForSubscription = () =>
+  store.dispatch(enhancedApi.endpoints.centrifugoSubscribeCreate.initiate());
+
+export const dispatch = store.dispatch;
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
