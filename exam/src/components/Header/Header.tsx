2bb1e362aa1a21a14cc655d344e30e2994155dd8
margo <m.yunanova@gmail.com>
2024-12-23 22:10:41 +0300

    feat: add markup for header

---
+++ exam/src/components/Header/Header.tsx
@@ -0,0 +1,67 @@
+import { Box, Typography } from '@mui/material';
+import HistoryIcon from '@mui/icons-material/History';
+import TranslateIcon from '@mui/icons-material/Translate';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../shared/routes';
+import styles from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      sx={{ maxWidth: 1200 }}
+      gap="10px"
+    >
+      <Box borderBottom="1px solid lightgray">
+        <Typography variant="h6" color="textSecondary">
+          VK Translate
+        </Typography>
+      </Box>
+      <Box display="flex" flexDirection="row" gap="10px">
+        <Link to={`/${ROUTES.TRANSLATE}`} className={styles.link}>
+          <Box
+            display="flex"
+            flexDirection="row"
+            gap="10px"
+            border="1px solid lightgray"
+            borderRadius="4px"
+            sx={{
+              backgroundColor: 'rgba(66, 133, 244, .12)',
+              cursor: 'pointer',
+            }}
+            component="button"
+            padding="10px 10px"
+            alignItems="center"
+          >
+            <TranslateIcon sx={{ color: 'rgb(25,103,210)' }} />
+            <Typography variant="subtitle2" sx={{ color: 'rgb(25,103,210)' }}>
+              Text
+            </Typography>
+          </Box>
+        </Link>
+        <Link to={`/${ROUTES.HISTORY}`} className={styles.link}>
+          <Box
+            display="flex"
+            flexDirection="row"
+            gap="10px"
+            border="1px solid lightgray"
+            borderRadius="4px"
+            sx={{
+              backgroundColor: 'rgba(66, 133, 244, .12)',
+              cursor: 'pointer',
+            }}
+            component="button"
+            padding="10px 10px"
+            alignItems="center"
+          >
+            <HistoryIcon sx={{ color: 'rgb(25,103,210)' }} />
+            <Typography variant="subtitle2" sx={{ color: 'rgb(25,103,210)' }}>
+              History
+            </Typography>
+          </Box>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
