c2a9c34d150942de3defcb2e3fb012faf081ef56
margo <m.yunanova@gmail.com>
2024-12-23 22:12:37 +0300

    feat: add Header into App

--- exam/src/App.tsx
+++ exam/src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Box } from '@mui/material';
 
 function App() {
-  return <Outlet />;
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      sx={{ flexGrow: 1, maxWidth: 1200 }}
+      gap="10px"
+    >
+      <Header />
+      <Outlet />
+    </Box>
+  );
 }
 
 export default App;
