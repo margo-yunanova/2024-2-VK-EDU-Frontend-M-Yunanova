ca5115d129cb08b6d8caa6ab60e83b6185ad3ca2
margo <m.yunanova@gmail.com>
2024-12-23 21:35:53 +0300

    fix: update vite config

--- exam/vite.config.ts
+++ exam/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '2024-2-VK-EDU-Frontend-M-Yunanova/exam',
   plugins: [react()],
   resolve: {
     alias: {
